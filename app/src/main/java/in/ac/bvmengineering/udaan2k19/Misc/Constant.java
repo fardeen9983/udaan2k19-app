129c0bdb5b78b6da8d8a8a3690d1b14964716065
Fardeen Khan <khanfardeen9983@gmail.com>
2019-02-27 11:26:25 +0530

    Creating constants

--- app/src/main/java/in/ac/bvmengineering/udaan2k19/Misc/Constant.java
+++ app/src/main/java/in/ac/bvmengineering/udaan2k19/Misc/Constant.java
@@ -1,5 +1,11 @@
 package in.ac.bvmengineering.udaan2k19.Misc;
 
 public class Constant {
-
+    public static String AUTOMATIVE_PHILOSOPHER = "mechProd";
+    public static String BUILDER_OF_AZKABAN = "civil";
+    public static String CHAMBER_OF_CODERS = "cpit";
+    public static String HALF_WAVE_PRINCE = "ecel";
+//    public static String ORDER_OF_OHMS = "mech";
+//    public static String AUTOMATIVE_PHILOSOPHER = "mech";
+//    public static String AUTOMATIVE_PHILOSOPHER = "mech";
 }
